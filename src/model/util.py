558e20b27b5efe8b744f249a370ead84cb124fe1
Jesse Simpson <jesse.simpson36@gmail.com>
2018-04-16 16:46:33 -0400

    removed root dir files

--- src/model/util.py
+++
@@ -1,9 +0,0 @@
-class Struct():
-    def __init__(self, **dic):
-        self.__dict__.update(dic)
-
-    def __getitem__(self, key):
-        return self.__dict__[key]
-
-    def __repr__(self):
-        return '{' + ', '.join(['{}:{}'.format(key, self[key]) for key in sorted(self.__dict__.keys()) if key[0] != '_']) + '}'
