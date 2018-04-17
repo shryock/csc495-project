617df997e4d1e5722c149e2f3778d859e161b2da
Joseph Stanley <jwstanl3@ncsu.edu>
2018-04-16 22:56:28 -0400

    Added documentation for machine and game files and fixed a few minor errors

--- proj3/src/model/game.py
+++ proj3/src/model/game.py
@@ -6,15 +6,30 @@ ranks = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 suitCharacters = {"Hearts": "\u2665", "Spades": "\u2660", "Clubs": "\u2663", "Diamonds": "\u2666"}
 
 class Game():
+    """Game model using a board, rulebook, list of players, and dictionary of moves to manage state
+
+    Attributes:
+        name (str): Name of game
+        board (Board): Game board used for managing and displaying piles
+        rulebook (Rulebook): Rules of game used as guards in machine
+        players (list): List of players in game
+        moves (dict): Dictionary of moves with move names as keys and corresponding functions as values
+        showRounds (boolean): Boolean used for printing, or not printing, round numbers
+
+    Note:
+        The dictionary of moves should always contain a QUIT key with value set to the quitGame method,
+        unless a game does not allow the player such an option
+    """
     def __init__(self):
         self.name = 'Game'
         self.board = None
         self.rulebook = None
         self.players = []
         self.moves = {}
-
-        self.moveCount = 0
         self.showRounds = False
+
+        # The following attributes should be untouched and are used for management by a game machine
+        self.moveCount = 0
         self.invalid = False
         self.currentPlayer = -1
         self.playerMoves = []
@@ -22,50 +37,80 @@ class Game():
         self.quit = False
 
     def setup(self):
+        """Setup relevant game attributes and initial values
+
+        Note:
+            The setup method should be used instead of overriding constructor as to not remove any
+            important attributes needed for running the game machine
+        """
         raise NotImplementedError
 
     def goal(self):
+        """Prints generic win message and closes logger for program exit"""
         if not self.quit:
+            # Winning message only printed if a player did not quit
             Logger.log('Player {} has won the game'.format(self.currentPlayer + 1))
         self.closeLogger()
 
     def quitGame(self, move):
+        """Sets quit flag to true
+
+        Args:
+            move (Move): Representation of player-selected move
+        """
         self.quit = True
 
     def chooseMove(self):
+        """Asks player to select a move from the available move list"""
         self.selectedMove = self.players[self.currentPlayer].chooseMove()
 
     def executeMove(self):
+        """Executes player-selected move when valid"""
         move = self.playerMoves[self.selectedMove]
         if not self.playerIsHuman():
+            # Print move selected by AI so human players can see selection
             Logger.log(' ' * 4, repr(move), '\n')
+        # Execute move using type defined in moves dictionary
         self.moves[move.moveType](move)
 
     def playerIsHuman(self):
+        """Returns true if player is human, false if AI"""
         return self.players[self.currentPlayer].isA(HumanPlayer)
 
     def printBanner(self):
+        """Prints banner containing name of game"""
         Logger.log('-' * 50)
         Logger.log('|', self.name.center(48), '|')
         Logger.log('-' * 50, '\n')
 
     def printRound(self):
+        """Prints round number if game allows rounds to be displayed"""
+        # Round numbers are only printed after reaching the first player once per iteration
         if self.showRounds and self.currentPlayer == 0 and not self.invalid:
             roundNum = int(self.moveCount / len(self.players) + 1)
             Logger.log(' Round {} '.format(roundNum).center(50, '='), '\n')
 
     def printHand(self):
+        """Prints player hand if any cards present"""
         if len(self.players[self.currentPlayer].hand):
             Logger.log("Player {}'s Hand: {}\n".format(
                 self.currentPlayer + 1,
                 repr(self.players[self.currentPlayer].hand)))
 
     def printBoard(self):
+        """Prints game board for human players to analyze
+
+        Note:
+            Board layout and printing should be handled through the repr() method of a game's
+            associated Board class
+        """
         if self.playerIsHuman():
             Logger.log(repr(self.board), '\n')
 
     def printMoves(self):
+        """Retrieves possible moves for current player and prints list only for human players"""
         self.playerMoves  = self.rulebook.getMoves(self)
+        # Always add quit option to move list; can be overrided if game does not allow quits
         self.playerMoves += [Move(None, None, None, 'QUIT')]
         if self.playerIsHuman():
             for i in range(len(self.playerMoves)):
@@ -73,16 +118,27 @@ class Game():
             Logger.log('')
 
     def printQuit(self):
+        """Prints quit message and associated player"""
         Logger.log('Player {} has quit the game'.format(self.currentPlayer + 1))
 
     def printInvalidMoveMessage(self):
+        """Prints invalid move message"""
         Logger.log('Invalid move selection. Please try again.', '\n')
 
     def closeLogger(self):
+        """Closes logger, used at program exit"""
         Logger.close()
 
 
 class Card():
+    """Representation of ordinary card found in a 52-card deck
+
+    Attributes:
+        rank (str): Rank of card (e.g., 10, Jack, King, Queen)
+        suit (str): Suit of card (e.g., hearts, diamonds)
+        visible(boolean, optional): Visibility flag for card, used when printing
+    """
+
     def __init__(self, rank, suit, visible=False):
         self.rank = rank
         self.suit = suit
@@ -94,40 +150,77 @@ class Card():
 
     def __repr__(self):
         if self.visible:
+            # Returns string of rank and suit character if visible
             return '{}{}'.format(self.rank, suitCharacters[self.suit])
         else:
+            # Returns string of empty square brackets to represent face-down card
             return '[ ]'
 
     def face(self):
+        """Returns face value of card (i.e., rank and suit character)"""
         return '{}{}'.format(self.rank, suitCharacters[self.suit])
 
     def makeVisible(self):
+        """Sets visibility of card to true"""
         self.visible = True
 
     def makeInvisible(self):
+        """Sets visibility of card to false"""
         self.visible = False
 
 
 class Deck():
+    """Representation of 52-card deck, used for initializing piles and hands
+
+    Attributes:
+        cards (list): List of cards containing each combination of rank and suit
+
+    Note:
+        Deck is shuffled when initialized, thus, does not require further shuffling
+    """
+
     def __init__(self):
         self.cards = [Card(rank, suit) for suit in suits for rank in ranks]
         self.shuffle()
 
     def shuffle(self):
+        """Shuffles deck in random order"""
         random.shuffle(self.cards)
 
     def distributeCardsToPlayers(self, howManyEach, players):
+        """Distributes a given number of cards to each player in player list
+
+        Args:
+            howManyEach (int): Number of cards to distribute to each player
+            players (list): List of players able to receive cards
+        """
         for player in players:
             for card in range(howManyEach):
                 player.receiveCard(self.cards.pop())
 
     def distributeCardsToPiles(self, howManyEach, piles):
+        """Distributes cards to piles based off values in given list of integers
+
+        Args:
+            howManyEach (list): List of number of cards to distribute to each corresponding pile
+            piles (list): List of piles that will receive cards
+
+        Note:
+            Length of howManyEach list must be less than or equal to length of piles list
+        """
         for i in range(len(piles)):
             for card in range(howManyEach[i]):
                 piles[i].receiveCard(self.cards.pop())
 
 
 class Pile():
+    """Representation of collection of cards in game (e.g., deck, hand, waste)
+
+    Attributes:
+        name (str): Name of pile, used for printing moves
+        cards (list, optional): List of cards, may be added later from deck or other piles
+    """
+
     def __init__(self, name, cards=None):
         self.name = name
         self.cards = cards or []
@@ -146,47 +239,90 @@ class Pile():
             yield card
 
     def indexOf(self, card):
+        """Returns the index of the card whose face value matches the given string, or -1 otherwise
+
+        Args:
+            card (str): Face value of card to find in pile
+        Returns:
+            Index of card with matching face value, or -1 otherwise
+        """
         for i in range(len(self.cards)):
             if self.cards[i].face() == card:
                 return i
         return -1
 
     def remove(self, index):
+        """Removes card at given index, if within list boundaries
+
+        Args:
+            index (int): Index of card in pile
+        Returns:
+            Removed card, or None
+        """
         if index in range(len(self.cards)):
             return self.cards.pop(index)
         else:
             return None
 
     def receiveCard(self, card):
+        """Appends given card to pile
+
+        Args:
+            card (Card): Card to be added to pile
+        """
         self.cards.append(card)
 
     def top(self):
+        """Returns top card of pile"""
         return self.cards[-1]
 
     def giveOneCard(self, otherPile):
+        """Gives the top card of the pile to another pile
+
+        Args:
+            otherPile (Pile): Pile to receive top card
+        """
         otherPile.receiveCard(self.cards.pop())
 
     def giveSetOfCards(self, indexToGive, otherPile):
+        """Gives set of cards, beginning with given index, to another pile
+
+        Args:
+            indexToGive (int): Index representing start of cards to give
+            otherPile (Pile): Pile to receive set of cards
+        """
         remainingCards = len(self.cards) - indexToGive
+
+        # Removes remaining cards, ignoring out of bounds indexes by using range
         for i in range(remainingCards):
             otherPile.receiveCard(self.cards.pop(indexToGive))
 
     def makeAllCardsVisible(self):
+        """Sets visibility of all cards in pile to true"""
         for card in self.cards:
             card.makeVisible()
 
     def makeAllCardsInvisible(self):
+        """Sets visibility of all cards in pile to false"""
         for card in self.cards:
             card.makeInvisible()
 
     def isEmpty(self):
+        """Returns true if no cards in pile"""
         return len(self.cards) == 0
 
     def shuffle(self):
+        """Shuffles pile in random order"""
         random.shuffle(self.cards)
 
 
 class Board():
+    """Game board used for managing and printing piles
+
+    Attributes:
+        piles (list, optional): List of piles; can be added later during setup
+    """
+
     def __init__(self, piles=None):
         self.piles = piles or []
         self.setup()
@@ -195,10 +331,49 @@ class Board():
         raise NotImplementedError
 
     def setup(self):
+        """Sets up piles in game board, assigning pile names and any needed variables"""
+        raise NotImplementedError
+
+
+class Rulebook():
+    """Rules of game used as guards in machine; also used for finding possible moves for players
+
+    Note:
+        All functions should accept a game object as a parameter and return a boolean, apart from the getMoves()
+        method used for retrieving all possible moves for the current player
+    """
+
+    def true(self, game):
+        """Guard which always returns true"""
+        return True
+
+    def validMove(self, game):
+        """Guard for validating moves; move selection must be within the bounds of the player's move list"""
+        return game.selectedMove >= 0 and game.selectedMove < len(game.playerMoves)
+
+    def quit(self, game):
+        """Guard for determining whether a player has quit the game"""
+        return game.quit
+
+    def win(self, game):
+        """Guard for determining whether the game has been won"""
+        raise NotImplementedError
+
+    def getMoves(self, game):
+        """Returns list of possible moves for the current player"""
         raise NotImplementedError
 
 
 class Move():
+    """Representation of a possible move by the current player
+
+    Attributes:
+        cardIndex (int): Index of card to move in from pile
+        fromPile (Pile): Pile in which card is currently located
+        toPile (Pile): Pile which will receive the movable card
+        moveType (str): Type of move as defined in the game's move dictionary
+    """
+
     def __init__(self, cardIndex, fromPile, toPile, moveType):
         self.cardIndex = cardIndex
         self.fromPile = fromPile
@@ -206,6 +381,8 @@ class Move():
         self.moveType = moveType
 
     def __repr__(self):
+        # All additional game moves not listed below, along with their corresponding string representations,
+        # should be added as a switch case in the conditions below
         if self.moveType == 'DRAW':
             return 'Draw card from Deck'
         elif self.moveType == 'RESHUFFLE':
@@ -222,24 +399,13 @@ class Move():
                 self.toPile.name)
 
 
-class Rulebook():
-    def true(self, game):
-        return True
-
-    def validMove(self, game):
-        return game.selectedMove >= 0 and game.selectedMove < len(game.playerMoves)
-
-    def quit(self, game):
-        return game.quit
-
-    def win(self, game):
-        raise NotImplementedError
-
-    def getMoves(self, game):
-        raise NotImplementedError
-
-
 class Player():
+    """Representation of a player in a game
+
+    Attributes:
+        hand (Pile): Player's current hand of cards
+    """
+
     def __init__(self):
         self.hand = Pile('Hand')
 
@@ -247,20 +413,33 @@ class Player():
         return '{{type: {}, hand: {}}}'.format(self.__class__.__name__, repr(self.hand))
 
     def isA(self, type):
+        """Returns whether the player instance is of a particular type; useful for determining which players are human or AI"""
         return isinstance(self, type)
 
     def receiveCard(self, card):
+        """Appends given card to player hand"""
         raise NotImplementedError
 
     def chooseMove(self):
+        """Selects move from list of possible moves
+
+        Returns:
+            Selected move as index in player moves list
+        """
         raise NotImplementedError
 
     def chooseSuit(self):
+        """Selects suit form list of possible suits
+
+        Returns:
+            Selected suit as index in list of suits
+        """
         raise NotImplementedError
 
 
 class HumanPlayer(Player):
     def receiveCard(self, card):
+        # Cards of human players are made visible
         card.makeVisible()
         self.hand.receiveCard(card)
 
@@ -268,46 +447,57 @@ class HumanPlayer(Player):
         try:
             Logger.log('Choose your next move:')
             userInput = input()
+            # Attempt to force user input as integer
             selectedMove = int(userInput) - 1
             Logger.log(str(selectedMove + 1), printLog=False)
         except ValueError:
+            # Set input as invalid selection if error occurs
             selectedMove = -1
-            Logger.log(userInput)
+            Logger.log(userInput, printLog=False)
         Logger.log('')
         return selectedMove
 
     def chooseSuit(self):
+        # Prints suits as list of options
         suitOptions = ''
         for i in range(len(suits)):
             suitOptions += '{:>4}. {}\n'.format(i + 1, suitCharacters[suits[i]])
 
+        # Continue asking for selected suit until valid input received
         selectedSuit = -1
         while selectedSuit < 0:
             Logger.log(suitOptions, '\n', 'Choose suit to switch to:')
             try:
                 userInput = input()
+                # Attempt to force use input as integer
                 selectedSuit = int(userInput) - 1
                 Logger.log(str(selectedSuit + 1), printLog=False)
             except ValueError:
+                # Set input as invalid selection if error occurs
                 selectedSuit = -1
-                Logger.log(userInput)
+                Logger.log(userInput, printLog=False)
             Logger.log('')
 
             if not selectedSuit in range(0, 4):
+                # Print invalid selection message if index out of bounds
                 Logger.log('Invalid suit selection. Please try again.', '\n')
+                selectedSuit = -1
 
         Logger.log('Suit has been changed to {}\n'.format(suits[selectedSuit]))
         return selectedSuit
 
 class AIPlayer(Player):
     def receiveCard(self, card):
+        # Cards of AI players are made invisible
         card.makeInvisible()
         self.hand.receiveCard(card)
 
     def chooseMove(self):
+        # Always select first option in list of moves
         return 0
 
     def chooseSuit(self):
+        # Randomly select suit to play
         selectedSuit = random.choice(range(0, 4))
         Logger.log('Suit has been changed to {}\n'.format(suits[selectedSuit]))
         return selectedSuit
--- proj3/src/model/machine.py
+++ proj3/src/model/machine.py
@@ -2,6 +2,13 @@ from model.util import *
 from model.game import *
 
 class State():
+    """Representation of a state managed by a Machine
+
+    Attributes:
+        name (str): Name of state
+        trans (list): List of transitions stored as Structs with an end state and a guard
+    """
+
     def __init__(self, name):
         self.name = name
         self.trans = []
@@ -10,10 +17,25 @@ class State():
         return '{} => {}'.format(self.name, [tran.end.name for tran in self.trans])
 
     def addTransition(self, end, guard):
+        """Add transition from self to end state with a conditional guard
+
+        Args:
+            end (State): End state of transition
+            guard (function): Function that accepts a Game parameter and returns a boolean
+        Returns:
+            State instance, used for chaining method calls
+        """
         self.trans.append(Struct(end=end, guard=guard))
         return self
 
     def step(self, game):
+        """Makes transition from current state instance to end state, if possible
+
+        Args:
+            game (Game): Game instance to be passed around as payload, or game environment
+        Returns:
+            End state from transition, or current state if no transition possible
+        """
         for tran in self.trans:
             if tran.guard(game):
                 self.onExit(game)
@@ -21,37 +43,72 @@ class State():
                 return tran.end
         return self
 
-    def onEntry(self, game): pass
+    def onEntry(self, game):
+        """Called when transitioning from another state to current state
 
-    def onExit(self, game): pass
+        Args:
+            game (Game): Game instance in machine
+        """
+        pass
+
+    def onExit(self, game):
+        """Called when transitioning from current state to another state
+
+        Args:
+            game (Game): Game instance in machine
+        """
+        pass
 
     def quit(self):
+        """Returns true if state instance is a final state"""
         return False
 
 
 class EndState(State):
+    """Representation of final state of machine"""
+
     def quit(self):
         return True
 
 
 class StartState(State):
+    """Representation of start state of game machine"""
+
     def onEntry(self, game):
+        """Prints banner with name of game"""
         game.printBanner()
 
 
 class PlayerState(State):
+    """Representation of the decision making state for a player
+
+    Attributes:
+        machine (PlayerMachine): Machine used for managing player decisions
+    """
+
     def __init__(self, name, machine):
         self.name = name
-        self.machine = machine
         self.trans = []
+        self.machine = machine
 
     def onEntry(self, game):
+        """Sets current player to the player's index and runs player machine"""
         game.currentPlayer = int(self.name[1]) - 1
         self.machine.run(game)
 
 
 class PlayState(State):
+    """Representation of game loop in game machine"""
+
     def createPlayMachine(self, players, rulebook):
+        """Creates a machine that iterates through available players until a win or quit condition is met
+
+        Args:
+            players (list): List of players in a game
+            rulebook (Rulebook): Rulebook associated with current game; must have win and quit guards
+        Returns:
+            Machine of player states
+        """
         self.machine = Machine()
 
         for player in players:
@@ -63,6 +120,7 @@ class PlayState(State):
         win = self.machine.addState(EndState('WIN'))
         quit = self.machine.addState(QuitState('QUIT'))
 
+        # Add win and quit transitions to each player state, as well as a transition to the next player
         for i in range(len(states)):
             states[i].addTransition(win, rulebook.win)
             states[i].addTransition(quit, rulebook.quit)
@@ -71,11 +129,15 @@ class PlayState(State):
         return self.machine
 
     def onEntry(self, game):
+        """Runs generated machine of player states based off game"""
         self.createPlayMachine(game.players, game.rulebook).run(game)
 
 
 class MoveState(State):
+    """Representation of move selection for players"""
+
     def onEntry(self, game):
+        """Prints round, board, hand, and moves (if applicable) before allowing user to select a move"""
         game.printRound()
         game.printBoard()
         game.printHand()
@@ -84,29 +146,47 @@ class MoveState(State):
 
 
 class ValidMoveState(EndState):
+    """Representation of state reached when player move selection is valid"""
+
     def onEntry(self, game):
+        """Executes selected move and sets appropriate flags and counters"""
         game.invalid = False
         game.moveCount += 1
         game.executeMove()
 
 
 class InvalidMoveState(State):
+    """Representation of state reached when player move selection is invalid"""
+
     def onEntry(self, game):
+        """Sets invalid move flag and prints message indicating invalid state"""
         game.invalid = True
         game.printInvalidMoveMessage()
 
 
 class GoalState(EndState):
+    """Representation of end of game state"""
+
     def onEntry(self, game):
+        """Prints goal message at end of game"""
         game.goal()
 
 
 class QuitState(EndState):
+    """Representation of state reached when player selects to quit game"""
+
     def onEntry(self, game):
+        """Prints quit message before exiting"""
         game.printQuit()
 
 
 class Machine():
+    """Finite state machine used for managing and iterating through states and transitions
+
+    Attributes:
+        states (dict): Dictionary of states in machine, stored using name of state
+        start (State): Start state of machine
+    """
     def __init__(self):
         self.states = {}
         self.start = None
@@ -115,11 +195,20 @@ class Machine():
         return 'start: {}\n\n'.format(self.start.name) + '\n'.join([repr(self.states[key]) for key in self.states.keys()])
 
     def addState(self, state):
+        """Adds state to machine, overwriting that of the same name, if it exists
+
+        Args:
+            state (State): State to be added to machine
+        Returns:
+            The given state
+        """
+        # Sets start state as given state if no start state exists
         self.start = self.start or state
         self.states[state.name] = state
         return self.states[state.name]
 
     def run(self, game):
+        """Steps through states in machine until a final state is reached"""
         state = self.start
 
         state.onEntry(game)
@@ -129,12 +218,19 @@ class Machine():
 
 
 class GameMachine(Machine):
+    """Finite state machine representing the highest abstraction of a game, consisting of only a start state, play loop, and end goal
+
+    Attributes:
+        game (Game): Game model to be manipulated during machine run
+    """
+
     def __init__(self, game):
         self.game = game
         self.game.setup()
         self.createMachine()
 
     def createMachine(self):
+        """Creates generic game machine consisting of start, play, and goal states with single transitions"""
         self.states = {
             'START': StartState('START'),
             'PLAY':  PlayState('PLAY'),
@@ -142,19 +238,29 @@ class GameMachine(Machine):
         }
         self.start = self.states['START']
 
+        # Start state always transitions to play state on exit
         self.states['START'].addTransition(self.states['PLAY'], self.game.rulebook.true)
+        # Play state always transitions to goal state on exit
         self.states['PLAY'].addTransition(self.states['GOAL'], self.game.rulebook.true)
 
     def startGame(self):
+        """Starts game machine"""
         self.run(self.game)
 
 
 class PlayerMachine(Machine):
+    """Finite state machine representing the decision making steps of a player, including move selection and evaluation of the selected move
+
+    Attributes:
+        rulebook (Rulebook): Game rulebook used for evaluating validity of player move
+    """
+
     def __init__(self, rulebook):
         self.rulebook = rulebook
         self.createMachine()
 
     def createMachine(self):
+        """Creates generic player machine consisting of move selection, invalid move, and valid move states"""
         self.states = {
             'MOVE': MoveState('MOVE'),
             'INVALID': InvalidMoveState('INVALID'),
@@ -162,6 +268,9 @@ class PlayerMachine(Machine):
         }
         self.start = self.states['MOVE']
 
+        # Transition from move selection state to valid move state based on rulebook evaluation
         self.states['MOVE'].addTransition(self.states['VALID'], self.rulebook.validMove)
+        # Transition from move selection state to invalid move state if rulebook does not accept move
         self.states['MOVE'].addTransition(self.states['INVALID'], self.rulebook.true)
+        # Always transition from invalid move state to move selection state
         self.states['INVALID'].addTransition(self.states['MOVE'], self.rulebook.true)
